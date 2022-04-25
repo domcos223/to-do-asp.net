e804076b7053ac392dd2c678b6c0c68b70e25199
domcos223 <horvath.dominka223@gmail.com>
2022-04-25 15:14:53 +0200

    update Program.cs
    creating Database with given context, EnsureCreated to check if the database was created previously

--- TaskManagerApi/TaskManagerApi/Program.cs
+++ TaskManagerApi/TaskManagerApi/Program.cs
@@ -30,6 +30,15 @@ else
     app.UseMigrationsEndPoint();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<TaskManagerContext>();
+    context.Database.EnsureCreated();
+    // DbInitializer.Initialize(context);
+}
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
